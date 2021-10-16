50a39ba98efc1cadea181ce67af79cb229a465f6
ysKim <znxkznxk1030@gmail.com>
2021-10-16 21:30:16 +0530

    Day - 17 | Field

--- src/components/post-new.jsx
+++ src/components/post-new.jsx
@@ -3,10 +3,19 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 
 class PostNew extends Component {
+  renderTitleField(field) {
+    console.log(field)
+    return (
+      <div>
+        <input {...field.input} />
+      </div>
+    )
+  }
+
   render() {
     return (
       <form>
-        <Field name="title" component={}></Field>
+        <Field name="title" component={this.renderTitleField}></Field>
       </form>
     )
   }
